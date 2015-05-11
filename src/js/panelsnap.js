a20accae8927ef8763421c130b06897b39336776
selbekk <kristofer.selbekk@bekk.no>
2015-05-11 21:22:36 +0200

    Add some content and a few more styles

--- src/js/panelsnap.js
+++ src/js/panelsnap.js
@@ -1,14 +1,12 @@
-jQuery(function($) {
+$(function() {
     var opts = {
-        $menu: $('.site-nav'),
-        directionThreshold: 200,
-        easing: 'swing',
-        keyboardNavigation: {
-            enabled: true,
-            wrapAround: false
-        },
-        menuSelector: '.site-nav-link',
-        offset: 40
+        //$menu: $('.site-nav-list'),
+        //menuSelector: '.site-nav-link',
+        //panelSelector: '> .fullsize-panel',
+        offset: 40,
+        directionThreshold: 40,
+        onSnapStart: function(e) { console.log('start', e); },
+        onSnapFinish: function(e) { console.log('end', e); }
     };
     $('.site-content').panelSnap(opts);
 });
